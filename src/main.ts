ea7c32a43e8afbaa205dc5afde125566e653da4f
Dracowyn <dracowyn@outlook.com>
2023-10-28 23:36:49 +0800

    执行了核弹，恢复main.ts

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import './assets/main.scss'
+import { createApp } from 'vue'
+// @ts-ignore
+import App from './App.vue'
+// 暗黑模式
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './assets/css-vars.scss'
+
+const app = createApp(App)
+
+// 国际化
+import i18n from "./locales/i18n";
+app.use(i18n)
+
+import VueKeycloakJs from '@dsb-norge/vue-keycloak-js'
+// import Keycloak from "keycloak-js";
+// @ts-ignore
+const VITE_KEYCLOAK_SERVER = import.meta.env.VITE_KEYCLOAK_SERVER
+// @ts-ignore
+const VITE_KEYCLOAK_REALM = import.meta.env.VITE_KEYCLOAK_REALM
+// @ts-ignore
+const VITE_KEYCLOAK_CLIENT_ID = import.meta.env.VITE_KEYCLOAK_CLIENT_ID
+app.use(VueKeycloakJs, {
+    init: {
+        onLoad: 'check-sso'
+    },
+    config: {
+        url: VITE_KEYCLOAK_SERVER,
+        realm: VITE_KEYCLOAK_REALM,
+        clientId: VITE_KEYCLOAK_CLIENT_ID,
+    },
+})
+
+app.mount('#app')
