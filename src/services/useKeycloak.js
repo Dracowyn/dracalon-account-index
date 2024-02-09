30d475a09e084fc0df88c66e3984b740be00dfcd
Dracowyn <dracowyn@outlook.com>
2024-02-09 23:48:29 +0800

    改进代码

--- src/services/useKeycloak.js
+++ src/services/useKeycloak.js
@@ -2,17 +2,33 @@ export default {
 	data() {
 		return {
 			kcLoading: true,
+			kcButtonDisabled: true,
 		};
 	},
 	mounted() {
-		this.$watch("$keycloak.ready", (ready) => {
+		const onKeycloakReady = (ready) => {
 			if (ready) {
+				this.kcButtonDisabled = false;
 				this.kcLoading = false;
 			}
-		});
+		};
 
+		// 监听 $keycloak.ready 变化
+		this.$watch("$keycloak.ready", onKeycloakReady);
+
+		// 如果 $keycloak 已经准备就绪，则立即执行操作
 		if (this.$keycloak.ready) {
-			this.kcLoading = false;
+			onKeycloakReady(true);
 		}
+
+		// 设置超时时间，超时后取消加载状态
+		const kcTimeout = setTimeout(() => {
+			this.kcLoading = false;
+			clearTimeout(kcTimeout);
+		}, 5000);
+	},
+	beforeUnmount() {
+		// 在组件销毁前取消监听
+		this.$watch("$keycloak.ready", null);
 	},
 };
